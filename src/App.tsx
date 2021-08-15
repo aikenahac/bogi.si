1f488f2f2a8d06d2ce73f5d2bdbc0b5e720d2b0a
Aiken Tine Ahac <30961404+aikenahac@users.noreply.github.com>
2021-08-15 19:43:48 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="title disable-select">Prsežm da si res bogi.</h1>
-        <p className="text disable-select">Ja ne vem kaj tuki dt, <a href="https://github.com/aikenahac/bogi.si/issues">piš mi recommendation na github issue</a></p>
+        {/*<p className="text disable-select">Ja ne vem kaj tuki dt, <a href="https://github.com/aikenahac/bogi.si/issues">piš mi recommendation na github issue</a></p>*/}
       </header>
     </div>
   );
